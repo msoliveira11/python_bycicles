fb9fd3da2722543e60221e8b0efa1a16d80bf703
MATHEUS SANTOS DE OLIVEIRA <matheuol@vm26889s20.corp.ad.emb>
2022-03-14 14:46:44 -0300

    Inserindo commit com solução final

--- rios_e_ilhas/algoritmo.py
+++ rios_e_ilhas/algoritmo.py
@@ -1,36 +1,70 @@
 #!/usr/bin/python3
 
-def encontrar_rios(matrix_mapa, linha, coluna, soma):
+import copy
 
-    # se encontrar terra
-    if (matrix[linha][coluna] == 0):
+def prRed(skk): print("\033[91m{}\033[00m" .format(skk), end="")
+
+# Mostrar passos?
+mostrar_passos = False
+
+# Definicoes de constantes
+CONST_RIO = 1
+CONST_ILHA = 0
+
+def imprimir_matriz(matriz, linha_elem, coluna_elem):
+    for lin in range(len(matriz)):
+        print("[", end="")
+        for col in range(len(matriz[0])):
+            if ((lin == linha_elem) and (col == coluna_elem)):
+                prRed(matriz[lin][col])
+            else:
+                print(str(matriz[lin][col]), end="")
+            if (col != (len(matriz[0])-1)):
+                print(",", end="")
+        print("]")
+    print()
+
+def rodar_algoritmo_busca(matrix_mapa, linha, coluna, evitado):
+   
+    # se sair do range da coluna
+    if ((len(matrix_mapa) <= linha) or (linha < 0)):
         return 0
 
     # se sair do range da linha
-    if (len(matrix[linha]) < coluna):
+    if ((len(matrix_mapa[linha]) <= coluna) or (coluna < 0)):
         return 0
 
-    # se sair do range da coluna
-    if (len(matrix) < linha):
+    # se encontrar terra
+    if (matrix_mapa[linha][coluna] == evitado):
         return 0
 
-    # andar para direita
-    soma += encontrar_rios(matrix_mapa,linha,coluna+1,soma)
+    matrix_mapa[linha][coluna] = evitado
 
-    # andar para baixo
-    soma += encontrar_rios(matrix_mapa,linha+1,coluna,soma)
+    if (mostrar_passos == True):
+        imprimir_matriz(matrix_mapa,linha,coluna)
 
-    # andar para esquerda
-    soma += encontrar_rios(matrix_mapa,linha,coluna-1,soma)
-
-    # andar para cima
-    soma += encontrar_rios(matrix_mapa,linha-1,coluna,soma)
-
-    return 1
-
-    return soma
+    return (1 + \
+            # andar para direita
+            rodar_algoritmo_busca(matrix_mapa,linha,coluna+1,evitado) + \
+            # andar para baixo
+            rodar_algoritmo_busca(matrix_mapa,linha+1,coluna,evitado) + \
+            # andar para esquerda
+            rodar_algoritmo_busca(matrix_mapa,linha,coluna-1,evitado) + \
+            # andar para cima
+            rodar_algoritmo_busca(matrix_mapa,linha-1,coluna,evitado))
 
 def encontrar_rios_e_ilhas(matrix_mapa):
     rios = []
     ilhas = []
+    soma_inicial = 0
+    matrix_original = copy.deepcopy(matrix_mapa)
+    for linha in range(len(matrix_mapa)):
+        for coluna in range(len(matrix_mapa[linha])):
+            tamanho_rio = rodar_algoritmo_busca(matrix_mapa, linha, coluna, CONST_ILHA)
+            if (tamanho_rio > 0):
+                rios.append(tamanho_rio)
+            else:
+                tamanho_ilha = rodar_algoritmo_busca(matrix_original, linha, coluna, CONST_RIO)
+                if (tamanho_ilha > 0):
+                    ilhas.append(tamanho_ilha)
     return rios, ilhas
--- rios_e_ilhas/test.py
+++ rios_e_ilhas/test.py
@@ -10,29 +10,80 @@ matrix1 = [ \
     [1, 0, 1, 1, 0],\
 ]
 
-gabarito1 = [1, 2, 2, 2, 5] 
+matrix2 = [ \
+        [1,0],\
+        [0,1],\
+        ]
+
+matrix3 = [ \
+        [1,1,0],\
+        [0,1,0],\
+        [0,1,1],\
+        ]
+
+mapas = []
+mapas.append(matrix1)
+mapas.append(matrix2)
+mapas.append(matrix3)
+
+gabarito_rios_1 = [1, 2, 2, 2, 5] 
+gabarito_rios_2 = [1, 1] 
+gabarito_rios_3 = [5]
+
+gabarito_rio = []
+gabarito_rio.append(gabarito_rios_1)
+gabarito_rio.append(gabarito_rios_2)
+gabarito_rio.append(gabarito_rios_3)
+
+gabarito_ilha_1 = [7, 5, 1]
+gabarito_ilha_2 = [1, 1]
+gabarito_ilha_3 = [2, 2]
+
+gabarito_ilha = []
+gabarito_ilha.append(gabarito_ilha_1)
+gabarito_ilha.append(gabarito_ilha_2)
+gabarito_ilha.append(gabarito_ilha_3)
+
+def ordenar_lista(entrada):
+    entrada.sort()
+    return entrada
 
 def comparar_se_valores_sao_iguais(valor1, valor2):
     if (valor1 == valor2):
         return True
     return False
 
-def verificar_rios_com_gabarito(rios, gabarito):
-    if (comparar_se_valores_sao_iguais(len(gabarito),len(rios)):
+def verificar_com_gabarito(lista, gabarito):
+    lista = ordenar_lista(lista)
+    gabarito = ordenar_lista(gabarito)
+    if (comparar_se_valores_sao_iguais(len(gabarito),len(lista))):
         for val in range(len(gabarito)):
-            if (!comparar_se_valores_sao_iguais(gabarito[val],rios[val])):
+            if ((comparar_se_valores_sao_iguais(gabarito[val],lista[val]))==False):
                 return False
         return True
     return False
 
+def realizar_testes(mapas, gabarito_rio, gabarito_ilha):
+    for teste in range(len(mapas)):
+        rios = []
+        ilhas = []
+        rios, ilhas = encontrar_rios_e_ilhas(mapas[teste])
+        print("Teste " + str(teste) + ":")
+        #print(rios)
+        #print(gabarito_rio[teste])
+        if (verificar_com_gabarito(rios,gabarito_rio[teste])):
+            print("Resultado de rios OK")
+        else:
+            print("Resultado de rios errado")
+        #print(ilhas)
+        #print(gabarito_ilha[teste])
+        if (verificar_com_gabarito(ilhas,gabarito_ilha[teste])):
+            print("Resultado de ilhas OK")
+        else:
+            print("Resultado de ilhas errado")
+
 def main():
-    rios = []
-    ilhas = []
-    rios, ilhas = encontrar_rios_e_ilhas(matrix1)
-    if (verificar_rios_com_gabarito(rios,gabarito1)):
-        print("Resultado OK")
-    else:
-        print("Resultado errado")
+    realizar_testes(mapas, gabarito_rio, gabarito_ilha)
 
 if __name__ == "__main__":
     main()
